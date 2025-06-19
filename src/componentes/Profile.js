f1caa778091d5220edc8ef6758534ce0e5389853
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-06-19 20:05:59 -0300

    Modifico el codigo y cambio

--- src/componentes/Profile.js
+++ src/componentes/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react'; 
 
 function Profile() {
   return (
@@ -6,6 +6,21 @@ function Profile() {
       <img src="https://i.imgur.com/5cKXnEl.png" alt="Perfil" width="80" />
       <h3>Ángela</h3>
       <button>Cerrar sesión</button>
+    </div>
+  );
+}
+
+export default Profile; */
+
+import React from 'react';
+import '../App.css'; // Si estás usando las clases de App.css
+
+function Profile() {
+  return (
+    <div className="profile">
+      <img src="https://i.pravatar.cc/150?img=3" alt="Foto de perfil" />
+      <h3>Nombre de Usuario</h3>
+      <button className="logout">Cerrar sesión</button>
     </div>
   );
 }
