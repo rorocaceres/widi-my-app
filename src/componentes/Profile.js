6d8a4544b5e7fd7b432b23114f3cfc3deb32ecf3
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-06-25 09:41:54 -0300

    Modifico el código de los botones

--- src/componentes/Profile.js
+++ src/componentes/Profile.js
@@ -1,17 +1,3 @@
-/*import React from 'react'; 
-
-function Profile() {
-  return (
-    <div className="profile">
-      <img src="https://i.imgur.com/5cKXnEl.png" alt="Perfil" width="80" />
-      <h3>Ángela</h3>
-      <button>Cerrar sesión</button>
-    </div>
-  );
-}
-
-export default Profile; */
-
 import React from 'react';
 import '../App.css'; // Si estás usando las clases de App.css
 
