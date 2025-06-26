3c650992a913b63b6b91e20a4ca499102d914e17
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-06-26 10:45:18 -0300

    Modifico el codigo por prolijidad

--- src/componentes/Sidebar.js
+++ src/componentes/Sidebar.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importar Link
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="sidebar fade-in">
       <Link to="/" className="sidebar-link">🏠 Inicio</Link>
       <Link to="/profesores" className="sidebar-link">👩‍🏫 Profesores</Link>
-      <p>📚 Cursos</p>
-      <p>📅 Turnos</p>
-      <p>✉️ Contacto</p>
+      <Link to="/cursos" className="sidebar-link">📚 Cursos</Link>
+      <Link to="/turnos" className="sidebar-link">📅 Turnos</Link>
+      <Link to="/contacto" className="sidebar-link">✉️ Contacto</Link>
     </div>
   );
 }
