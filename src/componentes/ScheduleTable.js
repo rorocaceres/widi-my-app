2d8cdb77c59cfc01359e996ef4ea566a7c7c8498
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-06-26 10:41:14 -0300

    Modifico el codigo para que sea compatible

--- src/componentes/ScheduleTable.js
+++ src/componentes/ScheduleTable.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import HorarioTabla from './HorarioTabla'; // importa el componente unificado
+
+function ScheduleTable() {
+  const columnas = ['Profesor', 'Materia', 'Martes', 'Miércoles', 'Jueves'];
+  const filas = [
+    ['Martínez', 'Matemática', '8:00–10:00', '8:00–18:00', '8:00–10:00'],
+    ['Gómez', 'Historia', '10:00–12:00', '10:00–12:00', '10:00–12:00'],
+    ['López', 'Química', '8:00–11:00', '8:00–11:00', '8:00–11:10'],
+    ['Rodríguez', 'Física', '9:00–11:00', '9:00–11:00', '9:00–11:10'],
+  ];
+
+  return <HorarioTabla columnas={columnas} filas={filas} />;
+}
+
+export default ScheduleTable;
+
+/*import React from 'react';
 
 function ScheduleTable() {
   return (
@@ -48,4 +65,4 @@ function ScheduleTable() {
   );
 }
 
-export default ScheduleTable;
+export default ScheduleTable;*/
