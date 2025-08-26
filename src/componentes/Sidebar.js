57306d9c190c1e2556bdf9a1059edae7a9028d8b
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-08-26 10:32:46 -0300

    Subo código modificado del js Sidebar

--- src/componentes/Sidebar.js
+++ src/componentes/Sidebar.js
@@ -5,13 +5,32 @@ import './Sidebar.css';
 function Sidebar() {
   return (
     <div className="sidebar fade-in">
-      <Link to="/" className="sidebar-link">🏠 Inicio</Link>
-      <Link to="/profesores" className="sidebar-link">👩‍🏫 Profesores</Link>
-      <Link to="/cursos" className="sidebar-link">📚 Cursos</Link>
-      <Link to="/turnos" className="sidebar-link">📅 Turnos</Link>
-      <Link to="/contacto" className="sidebar-link">✉️ Contacto</Link>
+      <Link to="/" className="sidebar-link"> Inicio</Link>
+      <Link to="/profesores" className="sidebar-link"> Profesores</Link>
+      <Link to="/cursos" className="sidebar-link"> Cursos</Link>
+      <Link to="/turnos" className="sidebar-link"> Turnos</Link>
+      <Link to="/horarios" className="sidebar-link"> Horarios</Link>
+      <Link to="/contacto" className="sidebar-link"> Contacto</Link>
     </div>
   );
 }
 
 export default Sidebar;
+/*import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar fade-in">
+      <NavLink to="/" className="sidebar-link" activeclassname="active">🏠 Inicio</NavLink>
+      <NavLink to="/profesores" className="sidebar-link" activeclassname="active">👩‍🏫 Profesores</NavLink>
+      <NavLink to="/cursos" className="sidebar-link" activeclassname="active">📚 Cursos</NavLink>
+      <NavLink to="/turnos" className="sidebar-link" activeclassname="active">📅 Turnos</NavLink>
+      <NavLink to="/horarios" className="sidebar-link" activeclassname="active">🕒 Horarios</NavLink>
+      <NavLink to="/contacto" className="sidebar-link" activeclassname="active">✉️ Contacto</NavLink>
+    </div>
+  );
+}
+
+export default Sidebar;*/
