dd90e76a2f29a1c27831283348308601e0f03b20
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-08-26 19:08:31 -0300

    .

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import "./Sidebar.css";
 
 export default function Sidebar() {
   const navigate = useNavigate();
