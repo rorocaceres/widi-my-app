34e8e7a21c167df85e5837066e5db5922dde16dd
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-08-27 12:07:38 -0300

    Trat de coregir

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Profile from "./Profile"; // asegura que esté importado
 
 export default function Inicio() {
   return (
     <div className="page-home">
-      {/* Contenedor central: tabla de ejemplo */}
       <div className="table-container">
         <table>
           <thead>
@@ -20,27 +18,14 @@ export default function Inicio() {
           <tbody>
             <tr>
               <td>08:00 - 09:00</td>
-              <td>Matemática</td>
-              <td>Lengua</td>
-              <td>Historia</td>
-              <td>Física</td>
-              <td>Inglés</td>
+              <td>Matemática</td><td>Lengua</td><td>Historia</td><td>Física</td><td>Inglés</td>
             </tr>
             <tr>
               <td>09:00 - 10:00</td>
-              <td>Química</td>
-              <td>Biología</td>
-              <td>Geografía</td>
-              <td>Matemática</td>
-              <td>Educación Física</td>
+              <td>Química</td><td>Biología</td><td>Geografía</td><td>Matemática</td><td>Ed. Física</td>
             </tr>
           </tbody>
         </table>
-      </div>
-
-      {/* Perfil a la derecha */}
-      <div className="profile">
-        <Profile />
       </div>
     </div>
   );
