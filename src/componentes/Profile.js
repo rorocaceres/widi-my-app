5cef5c8022acc4ed6239776353a2042d8cf3e474
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-08-27 11:35:46 -0300

    Corrijo

--- src/componentes/Profile.js
+++ src/componentes/Profile.js
@@ -27,24 +27,3 @@ function Profile() {
 
 export default Profile;
 
-/*import React from 'react';
-import './Profile.css';
-
-function Profile({ onLogout }) {
-  return (
-    <div className="profile-card fade-in">
-      <img
-        src="https://i.imgur.com/5cKXnEl.png"
-        alt="Perfil"
-        className="profile-img"
-      />
-      <h3 className="profile-name">Ángela</h3>
-      <button className="logout-btn" onClick={onLogout}>
-        Cerrar sesión
-      </button>
-    </div>
-  );
-}
-
-export default Profile;
-*/
