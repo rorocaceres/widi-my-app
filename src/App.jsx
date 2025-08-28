5a9c967e8535699a12a6e655c1c73caccecd727f
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-08-28 10:45:28 -0300

    subo codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 import Sidebar from "./componentes/Sidebar";
