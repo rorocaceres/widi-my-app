ae74ae29d5c5735afdb2dcebfea5d30dde7dec9c
rocaceres <caceres.rosario.epet.20@gmail.com>
2025-10-24 18:34:33 -0300

    Modifico el codigo

--- src/Firebase/client.js
+++ src/Firebase/client.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
+// Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBGf8c8PFilJR6yzICLyvqWZge2gplSqow",
   authDomain: "widi-my-app-f1dab.firebaseapp.com",
@@ -9,10 +11,16 @@ const firebaseConfig = {
   storageBucket: "widi-my-app-f1dab.firebasestorage.app",
   messagingSenderId: "920300071576",
   appId: "1:920300071576:web:666a4f528cf754cfd67eed",
-  measurementId: "G-V8P0CYC6LL"
+  measurementId: "G-V8P0CYC6LL",
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializar Analytics (opcional)
 const analytics = getAnalytics(app);
- //https://www.youtube.com/watch?v=scXYrAHSKJo
+
+// Inicializar Authentication
+export const auth = getAuth(app);
+
+export default app;
