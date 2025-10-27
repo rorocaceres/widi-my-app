8fbc58430f13a6f74ee99937b29c2994b95ab867
Antonella Blasco Hugo <antonellablascohugo14@gmail.com>
2025-10-27 14:57:08 -0300

    Intente modificar para que aparezca el nommbre en el perfil

--- src/componentes/Profile.js
+++ src/componentes/Profile.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./profile.css";
+import { auth } from "../Firebase/client";
 
 export default function Profile() {
+  const user = auth.currentUser; // ✅ Trae el usuario logueado
+
   return (
     <div className="profile-card">
-      <img src="https://i.imgur.com/5cKXnEl.png" alt="Perfil" className="profile-img" />
-      <h3 className="profile-name">Ángela</h3>
+      <img
+        src="https://i.imgur.com/5cKXnEl.png"
+        alt="Perfil"
+        className="profile-img"
+      />
+      {/* ✅ Muestra el nombre y correo reales */}
+      <h3 className="profile-name">
+        {user?.displayName || "Usuario sin nombre"}
+      </h3>
+      <p className="profile-email">{user?.email || "Sin correo"}</p>
     </div>
   );
 }
